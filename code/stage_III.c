8e789df566ddb2ed579fd7b76815cc268f1859c6
Bertie Wooster <bxwooster@gmail.com>
2012-11-07 10:53:56 +0400

    to_planet_GLstate

--- code/stage_III.c
+++ code/stage_III.c
@@ -35,10 +35,7 @@ stage_III (
                 & state
         );
 
-        GLenum poly_mode = state.show_wireframe ? GL_LINE : GL_FILL;
-        glPolygonMode(GL_FRONT_AND_BACK, poly_mode);
-        glClearColor (0.0, 0.0, 0.0, 0.0);
-        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        to_planet_GLstate (& state, gl);
 
         struct sysplanet * item;
         TAILQ_FOREACH(item, planetlist, _) {
---
+++ code/to_planet_GLstate.c
@@ -0,0 +1,11 @@
+void
+to_planet_GLstate (
+        struct framestate const * state,
+        struct GL * gl
+) {
+    (void) gl;
+    GLenum poly_mode = state->show_wireframe ? GL_LINE : GL_FILL;
+    glPolygonMode(GL_FRONT_AND_BACK, poly_mode);
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
