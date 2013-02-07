5c9f248b9ca8c9be9663b948799cad3cd7efae90
Bertie Wooster <bxwooster@gmail.com>
2013-02-07 17:29:40 +0400

    IP detection via recvfrom

--- code/federer/main.c
+++ code/federer/main.c
@@ -14,13 +14,17 @@ main (
     SOCKET sock = prepare_queriee_socket (&lib);
 
     unsigned long number;
-    int status = recv (sock, (void *) &number, sizeof (number), 0);
+    struct sockaddr_in source = {0};
+    int source_length = sizeof (source);
+
+    int status = recvfrom (sock, (void *) &number, sizeof (number), 0,
+            (void *) &source, &source_length);
     if (status < 0) {
         log_info("Recv has it a bit wrong. %s!", strerror (socket_errno ()));
         goto end;
     }
 
-    log_info ("Number %x!", number);
+    log_info ("Number %x from IP %s!", number, inet_ntoa (source.sin_addr));
 
 end:
     close_socket (sock);
