d58876861847e2c9bf66e5d4b7e97f7d0c49c246
Bertie Wooster <bxwooster@gmail.com>
2013-02-14 23:11:29 +0400

    Backdrop is round and blue now

--- code/cosmos/moduleC.c
+++ code/cosmos/moduleC.c
@@ -12,5 +12,11 @@ void moduleC (
     glEnableVertexAttribArray (shader->Apos2d);
 
     glUniformMatrix4fv (shader->Umvp, 1, GL_FALSE, framedata->viewproj.p);
+
+    vec3 center = {0};
+    vec3 colour = {0.1, 0.2, 0.3};
+    glUniform3fv (shader->Ucolour, 1, colour.p);
+    glUniform2fv (shader->Ucenter, 1, center.p);
+    glUniform1f (shader->Uradius, 1.0f);
     glDrawArrays (GL_TRIANGLES, 0, E->gl->vertices);
 }
--- code/cosmos/structures.h
+++ code/cosmos/structures.h
@@ -45,6 +45,9 @@ struct glts_cello {
     GLuint program;
     GLuint Apos2d;
     GLint Umvp;
+    GLint Ucolour;
+    GLint Ucenter;
+    GLint Uradius;
 };
 
 struct GL {
