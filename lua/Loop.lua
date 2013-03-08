382feaa9a7bd7a77952c9c33ca3ea4baf0fbac42
Bertie Wooster <bxwooster@gmail.com>
2013-03-08 13:49:30 +0400

    Blank slate.

--- lua/Loop.lua
+++ lua/Loop.lua
@@ -1,22 +1,12 @@
-core.Xstone_frame_G1 (E);
-core.Xstone_frame_G2 (E);
-core.Xstone_frame_G3 (E);
-core.Xstone_frame_C (E);
+--core.Xstone_frame_G1 (E);
+--core.Xstone_frame_G2 (E);
+--core.Xstone_frame_G3 (E);
+--core.Xstone_frame_C (E);
 
-if (KeyDown (KEY.W)) then
-    wireframe = not wireframe
-    core.XSetWireframe (wireframe)
-end
-
-if (KeyDown (KEY.N)) then
-    normalview = not normalview
-    core.XSetNormalview (normalview)
+if (KeyDown (KEY.L)) then
+    REPL ()
 end
 
 if (KeyDown (KEY.Escape)) then
     core.XHalt ()
-end
-
-if (KeyDown (KEY.L)) then
-    REPL ()
 end
