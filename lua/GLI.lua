60c1b7c323ead602d392121dfbabee486aa0303f
Bertie Wooster <bxwooster@gmail.com>
2013-05-01 13:55:50 +0400

    Dynamic

--- lua/GLI.lua
+++ lua/GLI.lua
@@ -1,26 +1,27 @@
 GLI = {}
 -- GL Improvements
 
-local function NewThing (Gen, Del)
-	local function gc (X) Del (1, X) end
-	local X = FFI.new ("GLuint [1]")
-	Gen (1, X)
+local function NewThing (Gen, Del, N)
+	N = N or 1
+	local function gc (X) Del (N, X) end
+	local X = FFI.new ("GLuint [?]", N)
+	Gen (N, X)
 	return FFI.gc (X, gc)
 end
 
-function GLI.NewTexture ()
-	return NewThing (GL.GenTextures, GL.DeleteTextures)
+function GLI.NewTexture (N)
+	return NewThing (GL.GenTextures, GL.DeleteTextures, N)
 end
 
-function GLI.NewBuffer ()
-	return NewThing (GL.GenBuffers, GL.DeleteBuffers)
+function GLI.NewBuffer (N)
+	return NewThing (GL.GenBuffers, GL.DeleteBuffers, N)
 end
 
-function GLI.NewVertexArray ()
-	return NewThing (GL.GenVertexArrays, GL.DeleteVertexArrays)
+function GLI.NewVertexArray (N)
+	return NewThing (GL.GenVertexArrays, GL.DeleteVertexArrays, N)
 end
 
-function GLI.NewQuery ()
-	return NewThing (GL.GenQueries, GL.DeleteQueries)
+function GLI.NewQuery (N)
+	return NewThing (GL.GenQueries, GL.DeleteQueries, N)
 end
 
--- lua/Prototype.lua
+++ lua/Prototype.lua
@@ -1,4 +1,4 @@
-function Prototype ()
+function Gen ()
     GL.DepthMask (GL.TRUE)
     GL.Enable (GL.DEPTH_TEST)
     GL.Disable (GL.BLEND)
@@ -6,26 +6,43 @@ function Prototype ()
 
 	GL.BindVertexArray (VAO[0])
 
-	Single (vec3 (-0.5, -0.5, -0.5))
-	Single (vec3 (-0.5,  0.5, -0.5))
-	Single (vec3 ( 0.5,  0.5, -0.5))
-	Single (vec3 ( 0.5, -0.5, -0.5))
+	centers = {
+		[0] = vec3 (-0.5, -0.5, 0),
+		vec3 (-0.5, 0.5, 0),
+		vec3 (0.5, 0.5, 0),
+		vec3 ( 0.5, -0.5, 0)
+	}
+
+	for i = 0, 3 do
+		First (i, centers[i])
+	end
 end
 
-function Single (center)
+Dynamic = true
+function Prototype ()
+	if KeyDown (KEY.M) then
+		Dynamic = not Dynamic
+		if not Dynamic then Gen () end
+	end
+	if Dynamic then Gen () end
+	for i = 0, 3 do
+		Second (i)
+	end
+end
+
+function First (I, center)
     GL.UseProgram (GMarching.glsl.program)
 
 	local uniform = GMarching.uniform
 	local grid = FFI.new ("int [3]")
-	local D = 32
+	local D = Dynamic and 32 or 128
+	print (Dynamic, D)
 	grid [0] = D
 	grid [1] = D
 	grid [2] = D
 	local total = grid[0] * grid[1] * grid[2]
-	local mvp = Sviewproj
 	local size = 1
 	local halfsize = vec3 (size / 2, size / 2, size / 2)
-    GL.UniformMatrix4fv (uniform.Umvp, 1, GL.FALSE, mvp.p)
     GL.Uniform3iv (uniform.grid, 1, grid)
     GL.Uniform3fv (uniform.vol0, 1, (center - halfsize).p)
     GL.Uniform3fv (uniform.vol1, 1, (center + halfsize).p)
@@ -36,24 +53,26 @@ function Single (center)
 	GL.BindTexture (GL.TEXTURE_BUFFER, TEdge[0])
 	GL.ActiveTexture (GL.TEXTURE1)
 	GL.BindTexture (GL.TEXTURE_3D, TNoise[0])
-	GL.BindBufferBase (GL.TRANSFORM_FEEDBACK_BUFFER, 0, BFeedback[0])
+	GL.BindBufferBase (GL.TRANSFORM_FEEDBACK_BUFFER, 0, BFeedback[I])
 
-	GL.BeginQuery (GL.TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN, QFeedback[0])
+	GL.BeginQuery (GL.TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN, QFeedback[I])
 	GL.Enable (GL.RASTERIZER_DISCARD)
 	GL.BeginTransformFeedback (GL.TRIANGLES)
     GL.DrawArrays (GL.POINTS, 0, total)
 	GL.EndTransformFeedback ()
 	GL.Disable (GL.RASTERIZER_DISCARD)
 	GL.EndQuery (GL.TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN)
+end
 
+function Second (I)
 	local uint = FFI.new ("uint32_t [1]")
-	GL.GetQueryObjectuiv (QFeedback[0], GL.QUERY_RESULT, uint)
-	print (uint[0])
+	GL.GetQueryObjectuiv (QFeedback[I], GL.QUERY_RESULT, uint)
 
     GL.UseProgram (GRender.glsl.program)
 	local uniform = GRender.uniform
+	local mvp = Sviewproj
     GL.UniformMatrix4fv (uniform.Umvp, 1, GL.FALSE, mvp.p)
-	GL.BindBuffer (GL.ARRAY_BUFFER, BFeedback[0])
+	GL.BindBuffer (GL.ARRAY_BUFFER, BFeedback[I])
 	GL.EnableVertexAttribArray (GRender.attribute.pos)
 	GL.EnableVertexAttribArray (GRender.attribute.normal)
 	local offset = FFI.cast ("char *", 0)
@@ -62,7 +81,7 @@ function Single (center)
 	GL.VertexAttribPointer (GRender.attribute.normal, 3, GL.FLOAT,
 		GL.FALSE, 28, offset + 16)
 
-	GL.DrawArrays (GL.TRIANGLES, 0, uint[0])
+	GL.DrawArrays (GL.TRIANGLES, 0, 3 * uint[0])
 
 	GL.DisableVertexAttribArray (GRender.attribute.pos)
 	GL.DisableVertexAttribArray (GRender.attribute.normal)
@@ -108,10 +127,12 @@ BEdge = GLI.NewBuffer ()
 GL.BindBuffer (GL.ARRAY_BUFFER, BEdge[0])
 GL.BufferData (GL.ARRAY_BUFFER, 1280 * 4, core.edge_data (), GL.STATIC_DRAW)
 
-BFeedback = GLI.NewBuffer ()
-GL.BindBuffer (GL.ARRAY_BUFFER, BFeedback[0])
-GL.BufferData (GL.ARRAY_BUFFER, 1048576, nil, GL.STREAM_DRAW)
-QFeedback = GLI.NewQuery ()
+BFeedback = GLI.NewBuffer (4)
+for i = 0, 3 do
+	GL.BindBuffer (GL.ARRAY_BUFFER, BFeedback[i])
+	GL.BufferData (GL.ARRAY_BUFFER, 2^25, nil, GL.STREAM_DRAW)
+end
+QFeedback = GLI.NewQuery (4)
 
 TEdge = GLI.NewTexture ()
 GL.BindTexture (GL.TEXTURE_BUFFER, TEdge[0])
