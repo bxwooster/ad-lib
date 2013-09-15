4b9c707c02516b5e86b53bf7026ef3427d10efe1
Bertie Wooster <bxwooster@gmail.com>
2013-09-15 17:58:52 +0400

    mc-new

--- lua/Dach.lua
+++ lua/Dach.lua
@@ -20,17 +20,17 @@ function Dach ()
 
 	local uniform = GCube.uniform
 	local mat = Sviewproj ^ mat4.Rotation (vec3 (1, 1, 1), Time)
-	local mat2 = Sviewproj ^ mat4.Rotation (vec3 (1, 1, 1), -Time)
+	local mat2 = Sviewproj ^ mat4.Rotation (vec3 (Time/100, 1, 1), Time)
     GL.UniformMatrix4fv (uniform.Umvp, 1, GL.FALSE, mat.p)
 
-	GL.DrawArrays (GL.TRIANGLES, 0, VCube.size)
+	--GL.DrawArrays (GL.TRIANGLES, 0, VCube.size)
 
 	NO_GL_WARNINGS = 1
 	local uniform = GMarching.uniform
 	GL.UseProgram (GMarching.glsl.program)
 	GL.Uniform1f (uniform.time, Time)
 	buffer, vertices =
-		MCGenerateBuffer (GMarching, 16, vec3 (-1,-1,-1), vec3(1,1,1))
+		MCGenerateBuffer (GMarching, 32, vec3 (-1,-1,-1), vec3(1,1,1))
 
 	MCDrawBuffer (buffer, vertices, mat2)
 	NO_GL_WARNINGS = 0
@@ -58,8 +58,12 @@ if not GMarching then
 		strs[1] = "colour"
 		GL.TransformFeedbackVaryings (program, 2, strs, GL.INTERLEAVED_ATTRIBS)
 	end
+	local main_shader = "glsl/marching-cubes.glts"
 	local function hot (null, file, text)
 		GMarching = LoadShader (file, text, cb)
 	end
-	core.Pull ("glsl/marching-cubes.glts", hot)
+	local function metahot (null, file, text)
+		core.Pull (main_shader, hot)
+	end
+	core.Pull ("glsl/mc-new.glsl", metahot)
 end
